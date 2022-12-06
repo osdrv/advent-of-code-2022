63c8cac57ec083fffdf412f55e487eaa4550d271
Oleg Sidorov <me@whitebox.io>
2022-12-06 09:13:47 +0100

    Update day 6 with a single-pass sliding window
    The initial I-just-woke-up-what's-up approach used a hashmap computed for every
    single window step. A better idea is to do counters with a sparse array.
    
    The performance improvement seems to be significant:
    
      Before optimization:
        time make
        go run common.go main.go
        make  7.80s user 0.19s system 99% cpu 8.010 total
    
      After Optimization:
        time make
        go run common.go main.go
        make  0.23s user 0.11s system 75% cpu 0.456 total

--- day-6/main.go
+++ day-6/main.go
@@ -17,6 +17,34 @@ func findStart(s string, k int) int {
 	return -ALOT
 }
 
+func findStartFast(s string, k int) int {
+	var sa [26]int
+	cnt := 0
+	ix := 0
+	for ix < k && ix < len(s) {
+		off := int(s[ix] - 'a')
+		sa[off]++
+		if sa[off] == 1 {
+			cnt++
+		}
+		ix++
+	}
+	for ix < len(s) && cnt != k {
+		off1 := int(s[ix-k] - 'a')
+		off2 := int(s[ix] - 'a')
+		sa[off1]--
+		if sa[off1] == 0 {
+			cnt--
+		}
+		sa[off2]++
+		if sa[off2] == 1 {
+			cnt++
+		}
+		ix++
+	}
+	return ix
+}
+
 func main() {
 	f, err := os.Open("INPUT")
 	noerr(err)
@@ -25,8 +53,9 @@ func main() {
 	lines := readLines(f)
 
 	for _, line := range lines {
-		pkgstrt := findStart(line, 4)
-		msgstrt := findStart(line, 14)
+		pkgstrt := findStartFast(line, 4)
+		msgstrt := findStartFast(line, 14)
+
 		printf("pkg start: %d", pkgstrt)
 		printf("msg start: %d", msgstrt)
 	}
